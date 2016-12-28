9addb5de74df7818a5bc6128754d0fcfaf728410
ikhlas firlana <ifirlana@gmail.com>
2016-12-29 00:04:46 +0700

    #1

---
+++ app/config/config_firebase.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ikhlasfirlana on 12/28/16.
+ */
+// var config = {
+//     apiKey: '<your-api-key>',
+//     authDomain: '<your-auth-domain>',
+//     databaseURL: '<your-database-url>',
+//     storageBucket: '<your-storage-bucket>'
+// };
+// firebase.initializeApp(config);
+angular.module("myApp", ["firebase"]);
