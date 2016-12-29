e3a98e789ed59403438060e29c2ff77a6137bce2
ikhlas firlana <ifirlana@gmail.com>
2016-12-29 11:59:25 +0700

    #2

--- app/config/config_firebase.js
+++ app/config/config_firebase.js
@@ -1,13 +1,13 @@
 /**
  * Created by ikhlasfirlana on 12/28/16.
  */
-var config = {
+var Config = {
     apiKey: "",
     authDomain: "",
     databaseURL: "",
     storageBucket: ""
 };
-firebase.initializeApp(config);
-angular
-    .module("App.config_firebase", ["firebase"])
-    .constant("Db", {});
+
+var Database = {
+    "DB": ""
+};
